d3c80c4438490ff381f6896214d907cafcba28eb
Nguyen Ngoc Thach <nguyenngocthach2301@gmail.com>
2026-09-11 22:20:29 +0700

    fix: resolve email worker startup and IPv6 issues

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -3,6 +3,6 @@ import dns from "node:dns";
 
 dns.setDefaultResultOrder("ipv4first");
 
-await import("./workers/order.worker.js");
+import "./workers/order.worker.js";
 
 console.log("Order Worker started...");
