cd51525ec010f2cb0c6cb008e1af292303bbf1ee
Nguyen Ngoc Thach <nguyenngocthach2301@gmail.com>
2026-09-11 22:08:50 +0700

    fix: edit nodemailer config for IPv4

--- lib/nodemailer.ts
+++ lib/nodemailer.ts
@@ -1,9 +1,14 @@
 import nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
+
   auth: {
     user: process.env.NODEMAILER_USER,
     pass: process.env.NODEMAILER_PASSWORD,
   },
-});
+} as SMTPTransport.Options);
